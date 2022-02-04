ae189646606be5f13f73d6625f7b04bbc4ed6d86
pedrolgsoares <pedrodevmob@gmail.com>
2022-02-04 20:41:53 -0300

    Repositorio criado

---
+++ lib/app/pages/todo_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class TodoPage extends StatelessWidget {
+  const TodoPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo list')),
+    );
+  }
+}
